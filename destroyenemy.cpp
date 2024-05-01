aafe288f4d6d86c15c745f80c71186d56e2b3249
Collaborators <66287008+jpotesta10@users.noreply.github.com>
2024-05-01 05:32:29 -0400

    Create destroyenemy.cpp

---
+++ destroyenemy.cpp
@@ -0,0 +1,13 @@
+#include ccjgnem
+
+//obtain facial recognition
+
+// track geo location
+
+//print shipping for drone and rover
+
+// deploy rover and drone
+
+//confirm identity
+
+//destroy target
